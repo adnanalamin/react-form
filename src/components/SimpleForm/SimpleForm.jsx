035b8a7601a7c23080b09b2153a68f883ae13592
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-03-21 22:11:55 +0600

    first commit

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,15 @@
+
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
